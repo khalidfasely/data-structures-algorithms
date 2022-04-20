fa65cfa0f1acb17382a0ec2f15d69b12e86d0f0a
khalidfasely <faselykhalid01@gmail.com>
2022-04-20 12:22:12 +0000

    Binary Search with Python

--- BinarySearch/binary-search.py
+++ BinarySearch/binary-search.py
@@ -1,4 +1,41 @@
-import math
+"""You're going to write a binary search function.
+You should use an iterative approach - meaning
+using loops.
+Your function should take two inputs:
+a Python list to search through, and the value
+you're searching for.
+Assume the list only has distinct elements,
+meaning there are no repeated values, and 
+elements are in a strictly increasing order.
+Return the index of value, or -1 if the value
+doesn't exist in the list."""
+
+def binary_search(input_array, value):
+    """Your code goes here."""
+    head = 0
+    tail = len(input_array) -1
+    pos = -1
+    while head <= tail:
+        mid = (head + tail) // 2
+        if input_array[mid] == value:
+            pos = mid
+            break
+        else:
+            if input_array[mid] < value:
+                head = mid +1
+            else:
+                tail = mid -1
+    return pos
+
+test_list = [1,3,9,11,15,19,29]
+test_val1 = 25
+test_val2 = 15
+print(binary_search(test_list, test_val1))
+print(binary_search(test_list, test_val2))
+
+
+"""First Try"""
+"""import math
 
 def indexOf(arr, value):
     not_finded = True
@@ -55,4 +92,4 @@ listB = [1, 2, 3, 4, 5, 6]
 #print(listB.index(5))
 #print(len(listB))
 print(listB.index(4))
-print(indexOf2(listB, 50))
+print(indexOf2(listB, 50))"""
